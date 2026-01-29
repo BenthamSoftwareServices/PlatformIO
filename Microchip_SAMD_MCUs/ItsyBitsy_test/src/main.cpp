f23fba90b4a0752042621d2361802aca193ff8cf
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-01-29 19:03:19 +0000

    Examining if delay() causes debugger problem

--- Microchip_SAMD_MCUs/ItsyBitsy_test/src/main.cpp
+++ Microchip_SAMD_MCUs/ItsyBitsy_test/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h> // Include Arduino core library
 #include <RTTStream.h> // Include RTTStream library, an alternative to Serial1 for real-time data transfer
 
-const long BAUD_RATE = 115200;
+//const long BAUD_RATE = 115200;
 
 RTTStream rtt;  // Create RTTStream object
 
@@ -9,22 +9,22 @@ int counter = 0; // Initialize counter variable
 
 void setup() {
   pinMode(13, OUTPUT); // Set pin 13 as output (usually connected to onboard LED) PA22 on MCU
-  delay(2000);  // Give debugger time to fully attach
-  Serial1.begin(BAUD_RATE); // Initialize Serial1 at BAUD_RATE
-  Serial1.print("\033[2J\033[H");  // Clear terminal screen and move cursor to home position
-  #ifdef DEBUG
-    Serial1.flush();  // Only flush when debugging, to ensure all data is sent
-  #endif
+  //delay(2000);  // Give debugger time to fully attach
+  //Serial1.begin(BAUD_RATE); // Initialize Serial1 at BAUD_RATE
+  //Serial1.print("\033[2J\033[H");  // Clear terminal screen and move cursor to home position
+  //#ifdef DEBUG
+    //Serial1.flush();  // Only flush when debugging, to ensure all data is sent
+  //#endif
   
 }
 
 void loop() {
   digitalWrite(13, HIGH);  // Red LED on
-  delay(500);
+  //delay(500);
   digitalWrite(13, LOW);   // Red LED off
-  delay(500);
+  //delay(500);
   counter++;
-
+/*
 Serial1.print("Hardware TX on PA17 ");
 #ifdef DEBUG
     Serial1.flush();  // Only flush when debugging
@@ -33,9 +33,10 @@ Serial1.print("Hardware TX on PA17 ");
 #ifdef DEBUG
     Serial1.flush();  // Only flush when debugging
   #endif
-
+  */
   //Use Real Time Transfer to stream data to RTT Viewer app on host PC
   rtt.print("Counter: ");  // Use RTTStream to print counter value
   rtt.print(counter); // Print the current counter value
   rtt.println(" - Hello world!"); // Print a message
+
 }
