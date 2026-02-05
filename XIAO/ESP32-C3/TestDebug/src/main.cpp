715a89332dbf937d9731ee7f5283e529276b4723
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-02-05 13:25:48 +0000

    Comments added

--- XIAO/ESP32-C3/TestDebug/src/main.cpp
+++ XIAO/ESP32-C3/TestDebug/src/main.cpp
@@ -1,6 +1,9 @@
 // Blink an external LED on D10
 // VITAL: Pin D8 must be pulled up to 3v3 via a 10K resistor, otherwise D8
 // floats and causes debugging to fail.
+// To start debugging first upload the code then click the debug icon, in the
+// left bar, and select PIO Debug (without uploading).  The normal F5 can also
+// be used but takes longer to launch.
 
 #include <Arduino.h>
 
