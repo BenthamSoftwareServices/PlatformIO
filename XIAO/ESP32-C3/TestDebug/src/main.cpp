b90678c2b871481e91393dd387f2e77241d30177
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-02-05 13:08:06 +0000

    counter variable added

--- XIAO/ESP32-C3/TestDebug/src/main.cpp
+++ XIAO/ESP32-C3/TestDebug/src/main.cpp
@@ -1,8 +1,13 @@
+// Blink an external LED on D10
+// VITAL: Pin D8 must be pulled up to 3v3 via a 10K resistor, otherwise D8
+// floats and causes debugging to fail.
+
 #include <Arduino.h>
 
 int ledPin = 10;
 int onDelay = 500;
 int offDelay = 1000;
+int32_t counter = 0;
 
 void setup() { pinMode(ledPin, OUTPUT); }
 
@@ -12,4 +17,6 @@ void loop()
     delay(onDelay);
     digitalWrite(ledPin, LOW);
     delay(offDelay);
+
+    counter++;
 }
