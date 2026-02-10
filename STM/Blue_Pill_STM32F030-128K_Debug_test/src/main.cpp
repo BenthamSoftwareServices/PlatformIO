0a0294808cdbb6d9ea83bcfa69fe1104c5e07d78
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-02-10 09:30:46 +0000

    Updates

---
+++ STM/Blue_Pill_STM32F030-128K_Debug_test/src/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * Blue Pill Onboard LED Blink
+ * Target: STM32F103C8 (128KB variant)
+ * LED Pin: PC13 (Active Low)
+ */
+
+#include <Arduino.h>
+#include <RTTStream.h>
+
+u_int32_t counter = 0;
+
+// PC13 is the standard location for the onboard LED on the Blue Pill
+const int ledPin = PC13;
+
+RTTStream rtt; // Initialise the RTT stream
+
+void setup()
+{
+    // Initialise the digital pin as an output
+    pinMode(ledPin, OUTPUT);
+    // No baud rate needed for RTT!
+    rtt.println("Blue Pill RTT Debugging Active");
+}
+
+void loop()
+{
+    digitalWrite(PC13, LOW);
+    rtt.print(counter);
+    rtt.println(" LED ON");
+    delay(1000);
+    digitalWrite(PC13, HIGH);
+    rtt.print(counter);
+    rtt.println(" LED OFF");
+    delay(1000);
+    counter++;
+}
