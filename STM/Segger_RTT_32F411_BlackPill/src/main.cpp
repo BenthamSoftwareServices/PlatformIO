6ddcdf5ce99601a4cc8c0263fbf5411a00b313e6
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-02-28 13:13:38 +0000

    Initial commit

--- STM/Segger_RTT_32F411_BlackPill/src/main.cpp
+++ STM/Segger_RTT_32F411_BlackPill/src/main.cpp
@@ -2,8 +2,8 @@
 // Target: STM32F411CE
 // LED Pin: PC13 (Active Low)
 
+#include "SEGGER_RTT.h"
 #include <Arduino.h>
-#include <RTTStream.h>
 
 #define ledPin                                                                 \
     PC13 // PC13 is the standard location for the onboard Blue LED on the mini
@@ -12,28 +12,26 @@
          // symbol "PC13" in current context. (from data-evaluate-expression
          // PC13)"
 
-u_int32_t counter = 0;
-
-RTTStream rtt; // Initialise the RTT stream
+u_int32_t counter =
+    0; // Initialise counter variable to keep track of loop iterations
 
 void setup()
 {
+    SEGGER_RTT_Init(); // Initialise the RTT system
     // Initialise the digital pin as an output
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, HIGH); // Turn LED off to set initial state
     // No baud rate needed for RTT!
-    rtt.println("RTT Debugging Active");
+    SEGGER_RTT_WriteString(0, "RTT Debugging Active\n");
 }
 
 void loop()
 {
     digitalWrite(ledPin, LOW); // Turn LED on
-    rtt.print(counter);
-    rtt.println(" LED ON");
-    delay(1500);
+    SEGGER_RTT_printf(0, "%d LED ON\n", counter);
+    delay(100);
     digitalWrite(ledPin, HIGH); // Turn LED off
-    rtt.print(counter);
-    rtt.println(" LED OFF");
-    delay(500);
+    SEGGER_RTT_printf(0, "%d LED OFF\n", counter);
+    delay(100);
     counter++;
 }
