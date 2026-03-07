575cdc647de19857d3350fb5547354432944d3ad
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-03-07 13:19:06 +0000

    updates

--- STM/Flash_PC13_LED_STM32F103C8/src/main.cpp
+++ STM/Flash_PC13_LED_STM32F103C8/src/main.cpp
@@ -1,13 +1,28 @@
+#include "SEGGER_RTT.h"
 #include <Arduino.h>
 
 #define LED_PIN PC13
 
-void setup() { pinMode(LED_PIN, OUTPUT); }
-
-void loop()
+void setup()
 {
-    digitalWrite(LED_PIN, LOW); // ON  (active low)
+    pinMode(LED_PIN, OUTPUT);
+    SEGGER_RTT_Init(); // Initialize RTT
+}
+
+void loop() {
+    digitalWrite(LED_PIN, LOW); // Turn ON LED (active low)
     delay(500);
-    digitalWrite(LED_PIN, HIGH); // OFF (active low)
+
+    // Read the pin state
+    int pinState = digitalRead(LED_PIN);
+    SEGGER_RTT_printf(0, "LED STATE: %d\n", pinState); // Print LED state
+    delay(500);
+    
+    digitalWrite(LED_PIN, HIGH); // Turn OFF LED (active low)
+    delay(500);
+
+    // Read the pin state again
+    pinState = digitalRead(LED_PIN);
+    SEGGER_RTT_printf(0, "LED STATE: %d\n", pinState); // Print LED state
     delay(500);
 }
