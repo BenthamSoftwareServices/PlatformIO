061c1380bccded93e527c1b1a6da21b83ba841c9
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-03-11 14:07:08 +0000

    updates

--- XIAO/ESP32-S3/Blink_LED/src/main.cpp
+++ XIAO/ESP32-S3/Blink_LED/src/main.cpp
@@ -2,9 +2,12 @@
 
 // If the Xiao becomes unresponsive press the boot button, pulse the reset
 // button, release the boot button and then re-upload the code. This will put
-// the Xiao into bootloader mode and allow you to upload new code to it.
+// the Xiao into bootloader mode and allow you to upload new code to it.  It may
+// be necessary to pulse the reset button again after uploading code to get the
+// Xiao to start running the new code.
 
 // XIAO ESP32-S3 Hardware Note: Onboard User LED is connected to GPIO21.
+
 // const int ledPin = 21;
 #define ledPin 21 // Define is preferred for constants in C++11 and later
 
