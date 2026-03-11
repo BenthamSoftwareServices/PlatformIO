26f083c32e5afb4a76fed8fd6b1b8e44e7ceb056
Steven Bennett <benthamsoftwareservices@gmail.com>
2026-03-11 14:04:47 +0000

    Initial commit

---
+++ XIAO/ESP32-S3/Blink_LED/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+// If the Xiao becomes unresponsive press the boot button, pulse the reset
+// button, release the boot button and then re-upload the code. This will put
+// the Xiao into bootloader mode and allow you to upload new code to it.
+
+// XIAO ESP32-S3 Hardware Note: Onboard User LED is connected to GPIO21.
+// const int ledPin = 21;
+#define ledPin 21 // Define is preferred for constants in C++11 and later
+
+void setup()
+{
+    // Configure GPIO21 as a digital output
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    // Set GPIO21 High (LED ON)
+    digitalWrite(ledPin, HIGH);
+    delay(100); // 1000ms pause
+
+    // Set GPIO21 Low (LED OFF)
+    digitalWrite(ledPin, LOW);
+    delay(100); // 1000ms pause
+}
--- XIAO/ESP32-S3/Flash_LED_debug_test_ESP32-S3/src/main.cpp
+++ XIAO/ESP32-S3/Flash_LED_debug_test_ESP32-S3/src/main.cpp
@@ -1,23 +1,22 @@
 #include <Arduino.h>
 
 // #define LED 21 //Built-in orange user LED on GPIO21 on XIAO ESP32-S3
-#define LED                                                                    \
-    D1 //External LED on pin D1, which is GPIO2, so D1 or just 2 flashes D1
+#define LED LED_BUILTIN // Built-in orange user LED on GPIO21 on XIAO ESP32-S3
 
 void setup()
 {
     // The following five lines cause execution to stop if the environment (env)
     // is set to env:DEBUG otherwise they are ignored if env:RELEASE is selected
-#ifdef DEBUG_SETUP
-    volatile int stop_here = 1;
-    while (stop_here) //Set a breakpoint at this line the spin loop in setup()
-        ;
-#endif
+     #ifdef DEBUG_SETUP
+       volatile int stop_here = 1;
+      while (stop_here) //Set a breakpoint at this line the spin loop in
+      //setup()    ;
+     #endif
 
     delay(100);
     pinMode(LED, OUTPUT);
     digitalWrite(LED, HIGH); // Turn on the LED
-    asm("nop"); //Use continue (F10) at this line if debugging
+    asm("nop");              // Use continue (F10) at this line if debugging
 }
 
 void loop()
